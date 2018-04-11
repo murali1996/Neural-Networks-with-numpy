4d3d321f7e46c82323be93864191c588626e62bf
murali1996 <s.jayanthi@corp.samsungelectronics.net>
2018-04-11 15:41:10 +0530

    batch_processing

--- libraries/models.py
+++ libraries/models.py
@@ -5,19 +5,19 @@
 # Models/Main function
 #==============================================================================
 import numpy as np
+from libraries import config
 from libraries import layers
 from libraries import activations
 from libraries import losses
 
 class Sequential(object):
     def __init__(self):
-        # RunTime
-        self.inputs = [];
-        self.targets = [];
-        self.layer_inputs = [];
-        self.layer_activation_inputs = [];
-        self.learning_rate = 0.001;
-        self.loss_values = [];
+        # RunTime- Forward pass
+        self.inputs, self.targets = [], [];
+        self.layer_inputs, self.layer_activation_inputs = [], [];
+        # RunTime- Backward pass
+        self.learning_rate = 0.002;
+        self.layer_delta_weights, self.layer_delta_biases = [], [];
         # Initializations
         self.layer_type, self.activation_type = [], [];
         self.layer_output_shapes = [];
@@ -37,27 +37,39 @@ class Sequential(object):
         self.layer_type.append(layer_type);
         self.activation_type.append(activation_type);
         self.layer_output_shapes.append(output_shape);
+        self.trainable_params_bool.append(trainable_params);
         self.layer_weights.append(weights);
         self.layer_biases.append(biases);
-        self.trainable_params_bool.append(trainable_params);
         return
     def compile(self, **keyword_args):
         self.loss_type = keyword_args['loss'];
         self.optimizer_type = keyword_args['optimizer']
         return
-    def train(self, inputs, targets, n_epochs=10):
-        self.inputs = inputs;
-        self.targets = targets;
-        batch_size = 1;
+    def train(self, inputs, targets, n_epochs=10, verbose=0):
+        self.inputs = inputs.astype(config.data_type);
+        self.targets = targets.astype(config.data_type);
+        batch_size = 2;
         for epoch in range(n_epochs):
-            print("Current Epoch: ", epoch);
-            thisEpochLoss = 0;
-            thisEpochIters = 0;
+            print("Epoch: {}/{}".format(epoch,n_epochs),end='\n');
+            # Initializations
+            thisEpochLoss = np.zeros(1, dtype=config.data_type);
+            thisEpoch_nBatches = int(inputs.shape[0]/batch_size);
             batch_start = 0;
             while(batch_start+batch_size<=inputs.shape[0]):
-                thisEpochIters+=1;
-#                untilThis = int(10*batch_start/inputs.shape[0]);
-#                print(">"*untilThis+"-"*(10-untilThis))
+                # Print Progress
+                nthBatch = int(batch_start/batch_size)+1;
+                if verbose:
+                    print('>'*nthBatch+'-'*(thisEpoch_nBatches-nthBatch), end='\r');
+                # FOR THIS BATCH: Initialize delta weights and delta biases
+                self.layer_delta_weights, self.layer_delta_biases = [], [];
+                for layer_ind in range(len(self.trainable_params_bool)):
+                    if self.trainable_params_bool[layer_ind]==True:
+                        self.layer_delta_weights.append(np.zeros(self.layer_weights[layer_ind].shape,dtype=config.data_type));
+                        self.layer_delta_biases.append(np.zeros(self.layer_biases[layer_ind].shape,dtype=config.data_type));
+                    else:
+                        self.layer_delta_weights.append(None);
+                        self.layer_delta_biases.append(None);
+                # FOR THIS BATCH: Forward and backward pass
                 for ind in range(batch_start, batch_start+batch_size):
                     # Forward Pass
                     currInput = inputs[ind];
@@ -79,26 +91,35 @@ class Sequential(object):
                         else:
                             self.layer_activation_inputs.append(None)
                     error = getattr(losses, self.loss_type)(currInput, currTarget, False)
-                    thisEpochLoss+=error;
-                    #print(error) # currInput is the output on which loss function has to be applied
-                    # Backward Pass
+                    error = error.astype(config.data_type);
+                    thisEpochLoss+=error; #print(error) # currInput is the output on which loss function has to be applied
+                    # Backward Pass # Collect delta values to be updated
                     currLoss = getattr(losses, self.loss_type)(currInput, currTarget, True)
                     for layer_ind in range(len(self.layer_type)-1, 0, -1): #Input layer need not be updated!
-                        if self.activation_type[layer_ind]!=None:
+                        if self.activation_type[layer_ind]!=None: # Pass loss through activation if any
                             act_type = self.activation_type[layer_ind];
                             currLoss *= getattr(activations, act_type, True)(self.layer_activation_inputs[layer_ind]);
-                        # Update Weights and biases
-                        if self.trainable_params_bool[layer_ind]==True:
+                        if self.trainable_params_bool[layer_ind]==True:  # Delta values collection for this one training sample in this batch
                             for i in range(self.layer_weights[layer_ind].shape[0]):
                                 for j in range(self.layer_weights[layer_ind].shape[1]):
-                                    self.layer_weights[layer_ind][i,j] -= self.learning_rate*self.layer_inputs[layer_ind][i]*currLoss[j];
+                                    self.layer_delta_weights[layer_ind][i,j] += (-1)*self.learning_rate*self.layer_inputs[layer_ind][i]*currLoss[j];
                             for j in range(self.layer_weights[layer_ind].shape[1]):
-                                self.layer_biases[layer_ind][j] -= self.learning_rate*currLoss[j];
+                                self.layer_delta_biases[layer_ind][j] += (-1)*self.learning_rate*currLoss[j];
                             # Pass on the currLoss
                             currLoss = np.dot(currLoss.T,self.layer_weights[layer_ind].T);
                             currLoss = currLoss.T;
+                # FOR THIS BATCH: Update Weights and biases
+                for layer_ind in range(len(self.layer_type)-1, 0, -1): #Input layer need not be updated!
+                    if self.trainable_params_bool[layer_ind]==True:
+                        for i in range(self.layer_weights[layer_ind].shape[0]):
+                            for j in range(self.layer_weights[layer_ind].shape[1]):
+                                self.layer_weights[layer_ind][i,j] += self.layer_delta_weights[layer_ind][i,j]
+                        for j in range(self.layer_weights[layer_ind].shape[1]):
+                            self.layer_biases[layer_ind][j] += self.layer_delta_biases[layer_ind][j];
+                # GO TO NEXT BATCH:
                 batch_start+=batch_size;
-            print(thisEpochLoss/thisEpochIters);
+            print('>'*thisEpoch_nBatches, end='\n');
+            print(thisEpochLoss/thisEpoch_nBatches);
         return
 
 
--- main.py
+++ main.py
@@ -6,12 +6,13 @@ Created on Tue Apr 10 17:21:58 2018
 import numpy as np
 
 from libraries.models import Sequential
-inputs = np.random.randn(100).reshape([10,10,1])
-targets = np.random.randint(0,2,(10,5,1))
+inputs = np.random.randn(1000).reshape([100,10,1])
+targets = np.random.randint(0,2,(100,5,1))
 model = Sequential();
 model.add('input_layer', 10);
 model.add('dense_layer', 32, 'relu');
 model.add('dense_layer', 64, 'relu');
+model.add('dense_layer', 128, 'relu');
 model.add('dense_layer', 32, 'relu');
 model.add('output_layer', 5);
 model.compile(loss = 'softmax_with_cross_entropy', optimizer='gradient_descent');
